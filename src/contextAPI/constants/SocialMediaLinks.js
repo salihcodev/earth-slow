1303ea9c60309258e598018b9303ee23f7bd46ca
Ahmed Salih <45488156+salihcodev@users.noreply.github.com>
2020-10-05 12:35:45 +0200

    Finish footer component. (#2)

---
+++ src/contextAPI/constants/SocialMediaLinks.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+import { IoLogoYoutube } from "react-icons/io";
+
+const SocialMediaLinks = [
+  { icon: <FaFacebookF />, link: "https://www.facebook.com" },
+  { icon: <FaTwitter />, link: "https://www.twitter.com" },
+  { icon: <FaLinkedinIn />, link: "https://www.linkedin.com" },
+  { icon: <FiInstagram />, link: "https://www.instagram.com" },
+  { icon: <IoLogoYoutube />, link: "https://www.youtube.com" },
+];
+
+export default SocialMediaLinks;
